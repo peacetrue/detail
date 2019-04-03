621564fb220b1a5b24f528cda7916afa3d5a9afb
xiayx <xiayouxue@hotmail.com>
2019-04-03 15:03:51 +0800

    value可通过name从数据中读取

--- src/main.js
+++ src/main.js
@@ -1,44 +1,17 @@
-let parentComponentMixIn = {
-    props: {
-        data: {type: Object},
-    },
-    provide() {
-        return {
-            $_detail_data: this.data,
-            $_detail_labelSpan: this.labelSpan,
-            $_detail_valueSpan: this.valueSpan,
-        };
-    },
-};
-
-let childComponentMixIn = {
-    props: {
-        data: {type: Object, default() {return this.$_detail_data}},
-        name: {type: String,},
-        value: {default() {if (this.data && this.name) return this.data[this.name];}}
-    },
-    inject: ['$_detail_data', '$_detail_labelSpan', '$_detail_valueSpan'],
-};
-
-
 /**详情容器*/
 Vue.component('detail', {
-    mixins: [parentComponentMixIn],
     props: {
-        data: {type: Object},
         labelSpan: {type: Number, default: 4},
         valueSpan: {type: Number, default: 8},
     },
     provide() {
         return {
-            $_detail_data: this.data,
             $_detail_labelSpan: this.labelSpan,
             $_detail_valueSpan: this.valueSpan,
         };
     },
     template: '<div class="detail"><slot></slot></div>'
 });
-
 
 /**详情头*/
 Vue.component('detail-header', {
@@ -47,26 +20,15 @@ Vue.component('detail-header', {
 
 /**详情项*/
 Vue.component('detail-item', {
-    mixins: [childComponentMixIn],
     props: {
         label: {type: String, required: true},
         labelSpan: {type: Number, default() {return this.$_detail_labelSpan}},
-        value: {type: [String, Boolean, Number, Array], default() {if (this.data && this.name) return this.data[this.name];}},
+        value: {type: [String, Boolean, Number, Array]},
         valueSpan: {type: Number, default() {return this.$_detail_valueSpan}},
     },
+    inject: ['$_detail_labelSpan', '$_detail_valueSpan'],
     template: `<div>
                     <i-col class="detail-item-label" :span="labelSpan">{{label}}</i-col>
                     <i-col class="detail-item-value" :span="valueSpan">{{value}}</i-col>
                 </div>`
-});
-
-/**详情表格*/
-Vue.component('detail-table', {
-    mixins: [childComponentMixIn],
-    props: {
-        label: {type: Object, required: true},
-        value: {type: Array, default() {if (this.data && this.name) return this.data[this.name];}}
-    },
-    inject: ['$_detail_data'],
-    template: '<i-table v-bind="label" v-bind:data="value"></i-table>'
 });
