41240915cbf7d82832d293f27286dc24241c486b
xiayx <xiayouxue@hotmail.com>
2019-04-03 14:40:39 +0800

    labelSpan和valueSpan从父容器继承

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 /**详情容器*/
 Vue.component('detail', {
+    props: {
+        labelSpan: {type: Number, default: 4},
+        valueSpan: {type: Number, default: 8},
+    },
+    provide() {
+        return {
+            $_detail_labelSpan: this.labelSpan,
+            $_detail_valueSpan: this.valueSpan,
+        };
+    },
     template: '<div class="detail"><slot></slot></div>'
 });
 
@@ -12,10 +22,11 @@ Vue.component('detail-header', {
 Vue.component('detail-item', {
     props: {
         label: {type: String, required: true},
-        labelSpan: {type: Number, default: 4},
-        value: {type: [String, Boolean, Number, Array], required: true},
-        valueSpan: {type: Number, default: 8},
+        labelSpan: {type: Number, default() {return this.$_detail_labelSpan}},
+        value: {type: [String, Boolean, Number, Array]},
+        valueSpan: {type: Number, default() {return this.$_detail_valueSpan}},
     },
+    inject: ['$_detail_labelSpan', '$_detail_valueSpan'],
     template: `<div>
                     <i-col class="detail-item-label" :span="labelSpan">{{label}}</i-col>
                     <i-col class="detail-item-value" :span="valueSpan">{{value}}</i-col>
